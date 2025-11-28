1d2bc54fea65f3240f70c148a983d08dcdb1715a
MaikazeShinobu <730toshinobu@gmail.com>
2025-11-28 23:03:32 +0900

    引き継げるところまで

--- docs/js/breadcrumb-labels.js
+++ docs/js/breadcrumb-labels.js
@@ -3,6 +3,23 @@ window.dirLabelMap = {
   "master": "データ管理",
 
   //中間ディレクトリ
+  
+  // master.html 追加分
+  "girltype": "キャストタグについて",
+  "cast": "キャスト一覧",
+  "customerlist": "顧客一覧",
+  "apply": "予約方法",
+  "payment": "支払方法",
+  "plan": "プラン",
+  "reservation": "指名料",
+  "option": "オプション",
+  "joining": "入会金",
+  "hotel": "ホテル",
+  "transportation": "交通費",
+  "discount": "割引",
+  "change": "チェンジ",
+  "extended": "延長",
+  "cost": "待機料・寮費・雑費",
 
   //データ管理用
   "add":"追加",
@@ -10,5 +27,5 @@ window.dirLabelMap = {
   "delete":"削除",
 
   //開発用
-  "sample":"サンプル"
+  "sample":"サンプル",
 };
